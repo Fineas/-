23c80a194523416cd0ec11ad7f2cd4b9d3969f93
FeDEX <fineas.silaghi@gmail.com>
2021-08-31 13:02:31 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source ../../common.sh
 
 make_input
 
-AFL_PRELOAD=./libc-2.32.so run_all # ud-2.32.so run_all
+AFL_PRELOAD=/root/libc_coverage/final_build/lib64/libc-2.32.so run_all # ld-2.32.so run_all
